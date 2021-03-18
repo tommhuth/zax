3bd14d76c4f0bdb52d1fe7287c20adca14904245
Tomm <tomm.huth@hotmail.com>
2021-03-18 23:50:11 +0100

    dimetric fix

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,15 +1,19 @@
-import {  useFrame, useThree } from "react-three-fiber"
+import { useFrame, useThree } from "react-three-fiber"
 import { useEffect, useLayoutEffect, useRef } from "react"
-import useStore from "../data/store"
+import useStore from "../data/store" 
 
 export default function Camera() {
     let { camera } = useThree()
     let playerPosition = useRef([0, 15, -40])
 
-    useLayoutEffect(() => {
-        // magic numbers!!, only works to right :/
-        camera.position.set(-10, 8.2925, -30) //set(-10, 8.2925, -30) // 8.2925
-        camera.lookAt(0, 0, -20)  // 20.17 
+    useLayoutEffect(() => {  
+        // https://discourse.threejs.org/t/dimetric-orthographic-camera-angle-for-retro-pixel-look/24455/2
+        camera.position.setFromSphericalCoords(
+            10,
+            -Math.PI / 3, // 60 degrees from positive Y-axis and 30 degrees to XZ-plane
+            Math.PI / 4  // 45 degrees, between positive X and Z axes, thus on XZ-plane
+        ) 
+        camera.lookAt(0, 0, 0)
     }, [camera])
 
     useEffect(() => {
