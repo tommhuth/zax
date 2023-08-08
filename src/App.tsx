f8e61bca3d1574d13bc3931449530b3f5309734f
Tomm <tomm.huth@hotmail.com>
2023-08-08 14:00:26 +0200

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -77,11 +77,11 @@ function App() {
 
             <mesh rotation-x={-Math.PI / 2} position-y={8} position-x={-7} >
                 <planeGeometry args={[12, 10000, 1, 1]} />
-                <meshBasicMaterial color="black" toneMapped={false} precision={"mediump"} />
+                <meshBasicMaterial color="black" toneMapped={false} />
             </mesh>
             <mesh rotation-x={-Math.PI / 2} position-y={8} position-x={23}>
                 <planeGeometry args={[12, 10000, 1, 1]} />
-                <meshBasicMaterial color="#000" toneMapped={false} precision={"mediump"} />
+                <meshBasicMaterial color="#000" toneMapped={false} />
             </mesh>
         </>
     )
