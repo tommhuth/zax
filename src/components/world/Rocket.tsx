b62f69ee6e7edec345ac11c79433620583db2633
Tomm <tomm.huth@hotmail.com>
2023-08-16 01:00:15 +0200

    rocket tweak

--- src/components/world/Rocket.tsx
+++ src/components/world/Rocket.tsx
@@ -1,8 +1,8 @@
-import { startTransition, useLayoutEffect, useRef } from "react"
+import { startTransition, useLayoutEffect, useMemo, useRef } from "react"
 import { Rocket } from "../../data/types"
 import { useInstance } from "../InstancedMesh"
 import { useFrame } from "@react-three/fiber"
-import { ndelta, setMatrixAt, setMatrixNullAt } from "../../data/utils/utils" 
+import { ndelta, setMatrixAt, setMatrixNullAt } from "../../data/utils/utils"
 import { Mesh, Vector3 } from "three"
 import random from "@huth/random"
 import { Tuple3 } from "../../types"
@@ -22,28 +22,46 @@ function explode(position: Vector3, size: Tuple3) {
             position.y + size[1] / 2,
             position.z,
         ],
+        count: [30, 50],
         size: [3, 6, 3]
     })
-    createParticles({
-        position: position.toArray(),
-        speed: [12, 16],
-        speedOffset: [[-5, 5], [-10, 10], [-15, 5]],
-        positionOffset: [[-1.5, 1.5], [-.5, .5], [-1.5, 1.5]],
-        normal: [0, -1, 0],
-        count: [10, 18],
-        radius: [.1, .45],
-        color: "#fff",
-    })
 
-    for (let direction of [-1, 1]) {
-        createExplosion({
-            position: [position.x, position.y - direction, position.z],
-            count: 10,
-            radius: .45,
-            fireballCount: 6,
-            fireballPath: [[position.x, position.y, position.z], [0, 3 * direction, 0]]
+    setTimeout(() => {
+        createParticles({
+            position: position.toArray(),
+            speed: [5, 10],
+            speedOffset: [[-10, 10], [-10, 10], [-10, 10]],
+            positionOffset: [[-1.5, 1.5], [-.5, .5], [-1.5, 1.5]],
+            normal: [0, 0, 0],
+            normalOffset: [[-1, 1], [-1, 1], [-1, 1]],
+            count: [16, 20],
+            radius: [.1, .45],
+            color: "#fff",
         })
+    }, 300)
+
+    let explosions = [
+        [125, [.2, size[1] / 2 - .2, .3], .2],
+        [0, [-.2, -size[1] / 2, -.25], .35]
+    ] as const
+
+    for (let [delay, [x, y, z], radius] of explosions) {
+        setTimeout(() => {
+            createExplosion({
+                position: [position.x + x, position.y + y, position.z + z],
+                count: 10,
+                radius,
+            })
+        }, delay)
     }
+
+    setTimeout(() => {
+        createExplosion({
+            position: [position.x, position.y, position.z],
+            count: 16,
+            radius: random.float(.6, .8),
+        })
+    }, 320)
 }
 
 export default function Rocket({
@@ -56,22 +74,20 @@ export default function Rocket({
     health,
 }: Rocket) {
     let grid = useStore(i => i.world.grid)
-    let removed = useRef(false)
-    let gravity = useRef(0)
+    let data = useMemo(() => {
+        return { removed: false, speed, triggerZ: 25, rotationY: random.float(0, Math.PI * 2) }
+    }, [speed])
     let ref = useRef<Mesh>(null)
-    let actualSpeed = useRef(speed)
-    let rotation = useRef<Tuple3>([0, random.float(0, Math.PI * 2), 0])
-    let triggerZ = useRef(25)
     let [rocketIndex, rocketInstance] = useInstance("rocket", { reset: false, color: "#FFF" })
     let remove = () => {
-        removed.current = true
+        data.removed = true
         increaseScore(500)
         removeRocket(id)
         setMatrixNullAt(rocketInstance, rocketIndex as number)
     }
 
-    useInstance("platform", { 
-        color: "#ddd", 
+    useInstance("platform", {
+        color: "#ddd",
         reset: false,
         position: [position.x, 0, position.z],
         rotation: [0, random.float(0, Math.PI * 2), 0]
@@ -80,40 +96,38 @@ export default function Rocket({
     useLayoutEffect(() => {
         if (health === 0) {
             startTransition(() => {
-                remove()
+                setTimeout(() => remove(), 450)
                 explode(position, size)
             })
         }
     }, [health])
-  
+
     useFrame((state, delta) => {
         let { player } = useStore.getState()
+        let d = ndelta(delta)
 
-        if (rocketInstance && typeof rocketIndex === "number" && !removed.current && player.object) {
-            if (health > 0) {
-                if (Math.abs(position.z - player.object.position.z) < triggerZ.current) {
-                    position.y += actualSpeed.current * ndelta(delta)
+        if (rocketInstance && typeof rocketIndex === "number" && !data.removed && player.object) {
+            if (Math.abs(position.z - player.object.position.z) < data.triggerZ) {
+                position.y += data.speed * d
 
-                    if (position.y > WORLD_TOP_EDGE + 1) {
-                        actualSpeed.current += 5 * ndelta(delta)
-                    }
+                if (health === 0) {
+                    data.speed -= .1
+                } else if (position.y > WORLD_TOP_EDGE + 2) {
+                    data.speed += .75
+                } else {
+                    data.speed += .01
                 }
-            } else {
-                gravity.current += 14 * ndelta(delta)
-                position.y -= gravity.current * ndelta(delta)
             }
 
             aabb.setFromCenterAndSize(position, _size.set(...size))
-
             setMatrixAt({
                 instance: rocketInstance,
                 index: rocketIndex,
                 position: position.toArray(),
-                rotation: rotation.current
+                rotation: [0, data.rotationY, 0]
             })
 
             ref.current?.position.set(...position.toArray())
-
             client.position = position.toArray()
             grid.updateClient(client)
         }
