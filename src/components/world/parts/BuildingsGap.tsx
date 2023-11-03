726c4016f3193c8a9ec2628cfe53c835d5844c3d
Tomm <tomm.huth@hotmail.com>
2023-11-03 23:39:59 +0100

    floor

--- src/components/world/parts/BuildingsGap.tsx
+++ src/components/world/parts/BuildingsGap.tsx
@@ -1,19 +1,27 @@
+import random from "@huth/random"
 import { WorldPartBuildingsGap } from "../../../data/types"
-import WorldPartWrapper from "../WorldPartWrapper"  
+import WorldPartWrapper from "../WorldPartWrapper"
 import Plane from "../spawner/Plane"
 import Turret from "../spawner/Turret"
+import Floor from "../Floor"
+import { WORLD_CENTER_X } from "../World"
 
 export default function BuildingsGap({
     id,
     position,
-    size, 
-}: WorldPartBuildingsGap) {  
+    size,
+}: WorldPartBuildingsGap) {
     return (
         <WorldPartWrapper
-            position={position} 
+            position={position}
             size={size}
             id={id}
-        > 
+        >
+            <Floor
+                position={[position.x + WORLD_CENTER_X, 0, position.z - size[1] / 2]}
+                scale={[random.pick(-1, 1), 1, .5 *  random.pick(-1, 1)]}
+                type="floor3"
+            />
             <Plane position={[3, 4, position.z - size[1]]} />
             <Plane position={[-3, 4, position.z - size[1] - 4]} />
             <Turret position={[0, 0, position.z - size[1] / 2]} />
